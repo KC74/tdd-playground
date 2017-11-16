c012df6a48fd239af828ba6214f44069ac9303b0
Kenneth Chow <kennethchow74@gmail.com>
2017-11-15 19:21:55 -0800

    updated algorithm to satisfy specs.

--- __tests__/string-compression.spec.js
+++ __tests__/string-compression.spec.js
@@ -1,17 +1,24 @@
 const stringCompression = require("../lib/string-compression.js");
 
 describe("stringCompression", () => {
-  describe("when string is aaabbbccc should return a3b3c3", () => {
+  describe("when string is aaabbbccc", () => {
     it("should return a3b3c3", () => {
       const result = stringCompression("aaabbbccc");
       expect(result).toEqual("a3b3c3");
     });
   });
 
-  describe("when string is ghjffg should return g1h1j2f2", () => {
+  describe("when string is ghjff", () => {
     it("should return g1h1j2f2", () => {
       const result = stringCompression("ghjff");
       expect(result).toEqual("g1h1j1f2");
+    });
+  });
+
+  describe("when string is aabccabbc", () => {
+    it("should return a2b1c2a1b2c1", () => {
+      const result = stringCompression("aabccabbc");
+      expect(result).toEqual("a2b1c2a1b2c1");
     });
   });
 });
